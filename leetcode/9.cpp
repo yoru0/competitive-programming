3281a45c9b930d706dee16f201f47792870b91d3
yoru <jordanchandra1@gmail.com>
2025-12-10 01:40:46 +0700

    .

--- leetcode/9.cpp
+++ leetcode/9.cpp
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
+    bool isPalindrome(int x)
+    {
         string y = to_string(x);
 
         int i = 0, j = y.size() - 1;
-        while (i < j) {
+        while (i < j)
+        {
             if (y[i] != y[j])
                 return false;
             i++;
             j--;
         }
-
         return true;
     }
 };
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
